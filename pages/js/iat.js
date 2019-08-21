46cf7696abf0c83e4a088ffa69f146abe2d58f7a
Jacco van den Berg <jaccoberg2281@gmail.com>
2019-08-21 14:48:34 +0200

    iat af

--- pages/js/iat.js
+++ pages/js/iat.js
@@ -1,7 +1,8 @@
 function  $(elementId) {return document.getElementById(elementId);}
 const scene = $('js--scene');
 const camera = $('js--camera');
-let color = "green";
+const cursor = $('js--cursor');
+let colorPaint = "none";
 
 cameraToSouthEast = () => {
     camera.setAttribute('position', '15 2 15');
@@ -20,25 +21,137 @@ cameraToNorthwest = () => {
 };
 
 colorDoor = () => {
-    $('js--door').setAttribute('mtl', '#deurBlue-mtl');
+    switch (colorPaint) {
+        case "none":
+            $('js--door').setAttribute('mtl', '#deur-mtl');
+            break;
+        case "yellow":
+            $('js--door').setAttribute('mtl', '#deurYellow-mtl');
+            break;
+        case "blue":
+            $('js--door').setAttribute('mtl', '#deurBlue-mtl');
+            break;
+        case "red":
+            $('js--door').setAttribute('mtl', '#deurRed-mtl');
+            break;
+        default:
+            $('js--door').setAttribute('mtl', '#deur-mtl');
+    }
+
 };
 
 colorSmallWall = () =>{
-    $('js--smollWall').setAttribute('mtl', '#muurZonderDeur-mtl');
+    switch (colorPaint) {
+        case "none":
+            $('js--smollWall').setAttribute('mtl', '#muurZonderDeur-mtl');
+            break;
+        case "yellow":
+            $('js--smollWall').setAttribute('mtl', '#muurZonderDeurYellow-mtl');
+            break;
+        case "blue":
+            $('js--smollWall').setAttribute('mtl', '#muurZonderDeurBlue-mtl');
+            break;
+        case "red":
+            $('js--smollWall').setAttribute('mtl', '#muurZonderDeurRed-mtl');
+            break;
+        default:
+            $('js--smollWall').setAttribute('mtl', '#muurZonderDeur-mtl');
+    }
 };
 
 colorSmollSmollWall = () =>{
-    $('js--smollSmollWall').setAttribute('mtl', '#korteMuur1-mtl');
+    switch (colorPaint) {
+        case "none":
+            $('js--smollSmollWall').setAttribute('mtl', '#korteMuur1-mtl');
+            break;
+        case "yellow":
+            $('js--smollSmollWall').setAttribute('mtl', '#muurZijkantenYellow-mtl');
+            break;
+        case "blue":
+            $('js--smollSmollWall').setAttribute('mtl', '#muurZijkantenBlue-mtl');
+            break;
+        case "red":
+            $('js--smollSmollWall').setAttribute('mtl', '#muurZijkantenRed-mtl');
+            break;
+        default:
+            $('js--smollSmollWall').setAttribute('mtl', '#korteMuur1-mtl');
+    }
 };
 
 colorSmollSmollWall2 = () =>{
-    $('js--smollSmollWall2').setAttribute('mtl', '#korteMuur1-mtl');
+    switch (colorPaint) {
+        case "none":
+            $('js--smollSmollWall2').setAttribute('mtl', '#korteMuur1-mtl');
+            break;
+        case "yellow":
+            $('js--smollSmollWall2').setAttribute('mtl', '#muurZijkantenYellow-mtl');
+            break;
+        case "blue":
+            $('js--smollSmollWall2').setAttribute('mtl', '#muurZijkantenBlue-mtl');
+            break;
+        case "red":
+            $('js--smollSmollWall2').setAttribute('mtl', '#muurZijkantenRed-mtl');
+            break;
+        default:
+            $('js--smollSmollWall2').setAttribute('mtl', '#korteMuur1-mtl');
+    }
 };
 
 colorBigWall = () =>{
-    $('js--bigWall').setAttribute('mtl', '#muur-mtl');
+    switch (colorPaint) {
+        case "none":
+            $('js--bigWall').setAttribute('mtl', '#muur-mtl');
+            break;
+        case "yellow":
+            $('js--bigWall').setAttribute('mtl', '#muurYellow-mtl');
+            break;
+        case "blue":
+            $('js--bigWall').setAttribute('mtl', '#muurBlue-mtl');
+            break;
+        case "red":
+            $('js--bigWall').setAttribute('mtl', '#muurRed-mtl');
+            break;
+        default:
+            $('js--bigWall').setAttribute('mtl', '#muur-mtl');
+    }
 };
 
 colorRoof = () =>{
     $('js--roof').setAttribute('mtl', '#rietDak-mtl');
+    switch (colorPaint) {
+        case "none":
+            $('js--roof').setAttribute('mtl', '#rietDak-mtl');
+            break;
+        case "yellow":
+            $('js--roof').setAttribute('mtl', '#rietDakYellow-mtl');
+            break;
+        case "blue":
+            $('js--roof').setAttribute('mtl', '#rietDakBlue-mtl');
+            break;
+        case "red":
+            $('js--roof').setAttribute('mtl', '#rietDakRed-mtl');
+            break;
+        default:
+            $('js--roof').setAttribute('mtl', '#rietDak-mtl');
+    }
+};
+
+selectDefault = () =>{
+    colorPaint = "none";
+    cursor.setAttribute('material', 'color: black; shader: flat')
+};
+
+selectRed = () =>{
+    colorPaint = "red";
+    cursor.setAttribute('material', 'color: red; shader: flat')
+};
+
+selectBlue = () =>{
+    colorPaint = "blue";
+    cursor.setAttribute('material', 'color: blue; shader: flat')
+};
+
+selectYellow = () =>{
+    colorPaint = "yellow";
+    cursor.setAttribute('material', 'color: yellow; shader: flat')
 };
